dd2face1d6c0c018d7113ce2b6818de2f2a41bfd
glen1986 <escobar.glen@gmail.com>
2021-10-04 13:21:51 -0300

    formularios

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,13 +1,21 @@
-const Display = ({obj}) => {
+const Display = ({myFruit}) => {
   return (
-    <table>
-      {obj.map((item, index) =>
-        <tr key={index}>
-          <td className="name">nome: {item.name}, </td>
-          <td className="qtd">qtd: {item.quantity} </td>
-          <td className="preco">preco: {item.price} </td>
-        </tr>)}
-    </table>
-  );
+    <>
+      {myFruit.length > 0 ? (
+        <table>
+          {console.log(myFruit)}
+          {[myFruit].map((item, index) =>
+            <tr key={index}>
+              <td className="name">nome: {item.name}, </td>
+              <td className="qtd">qtd: {item.quantity} </td>
+              <td className="preco">preco: {item.price} </td>
+            </tr>)}
+
+        </table>
+      ) : (
+        <></>
+      )}
+    </>
+  )
 };
 export default Display;
