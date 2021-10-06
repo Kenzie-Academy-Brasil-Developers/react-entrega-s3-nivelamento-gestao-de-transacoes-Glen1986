01a29a4ae7302092129d3a42dbc8c0f69eab75ac
glen1986 <escobar.glen@gmail.com>
2021-10-06 12:56:02 -0300

    styles

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,16 +1,17 @@
+import {Container} from './styles'
 const Display = ({listProd}) => {
   return (
-    <>
+    <Container>
       {listProd ? listProd.map((item, index) => (
         <tr key={index} >
-          <td className="name">nome: {item.name}  </td>
-          <td className="qtd">qtd: {item.quantity}  </td>
-          <td className="preco">preco: {item.price}  </td>
+          <td className="name"> nome: {item.name} </td>
+          <td className="qtd"> qtd: {item.quantity}  </td>
+          <td className="preco"> preco: {item.price} </td>
         </tr>
       )) : (
         <></>
       )}
-    </>
+    </Container>
   )
 };
 export default Display;
